9204f968864b6319780ab1f05c47445c8cbabc5c
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-24 22:53:48 +0530

    refactor :Updated Radius class name  to AppRadius

--- lib/res/style/radius.dart
+++ lib/res/style/radius.dart
@@ -1,6 +1,6 @@
 import 'package:flutter/material.dart';
 
-class Radius {
+class AppRadius {
   static BorderRadius smallRadius = BorderRadius.circular(10);
   static BorderRadius mediumRadius = BorderRadius.circular(20);
   static BorderRadius largeRadius = BorderRadius.circular(30);
