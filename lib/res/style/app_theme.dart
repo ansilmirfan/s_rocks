3c787af24ae50b4edd8bceb47b5d1f1198e82575
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-24 15:14:26 +0530

    feat : added Custom AppTheme in material app

---
+++ lib/res/style/app_theme.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+import 'package:s_rocks/res/style/colors_scheme.dart';
+
+class AppTheme {
+  static ThemeData themeData = ThemeData(
+    colorScheme: AppColorScheme.appColorScheme,
+  );
+}
