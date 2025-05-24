0cf718585570b1195511c99e0cb9ca8c5aceb2ee
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-24 22:56:52 +0530

    refactor(ui) : added appbar in home page

--- lib/view/home/home.dart
+++ lib/view/home/home.dart
@@ -1,10 +1,30 @@
 import 'package:flutter/material.dart';
+import 'package:s_rocks/utils/extensions/space_extension.dart';
+import 'package:s_rocks/view/home/widgets/search_field.dart';
+import 'package:s_rocks/view/widgets/svg_image.dart';
 
 class Home extends StatelessWidget {
   const Home({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(body: CustomScrollView(slivers: [_buildAppBar(context)]));
+  }
+
+  SliverAppBar _buildAppBar(BuildContext context) {
+    return SliverAppBar(
+      backgroundColor: Theme.of(context).colorScheme.primary,
+      title: SearchField(),
+      actions: [
+        CircleAvatar(
+          backgroundColor: Colors.white,
+          child: SvgImage(
+            assetPath: "assets/images/icons/avathar.svg",
+            color: Colors.deepPurple,
+          ),
+        ),
+        25.0.w,
+      ],
+    );
   }
 }
