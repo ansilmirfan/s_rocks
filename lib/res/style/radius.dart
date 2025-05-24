8169200d496c58f2490db0d1770e7d1b44f76bc4
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-24 20:14:02 +0530

    feat : added Radius class contains predefined radius for reusable codes

---
+++ lib/res/style/radius.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class Radius {
+  static BorderRadius smallRadius = BorderRadius.circular(10);
+  static BorderRadius mediumRadius = BorderRadius.circular(20);
+  static BorderRadius largeRadius = BorderRadius.circular(30);
+}
