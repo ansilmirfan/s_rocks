4e10e7b32c809cf631c2356e6ddd6311945db93b
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-24 12:44:26 +0530

    refactor : updated black color with new value

--- lib/res/style/app_colors.dart
+++ lib/res/style/app_colors.dart
@@ -3,7 +3,7 @@ import 'package:flutter/material.dart';
 class AppColors {
   static Color grey = Color(0xFF61616B);
   static Color white = Colors.white;
-  static Color black = Colors.black;
+  static Color black = Color(0xFF18171C);
   static Color transperent = Color(0xFF202126);
   static Color darkGrey = Color(0xFF2F2F39);
   static LinearGradient linearGradient = LinearGradient(
