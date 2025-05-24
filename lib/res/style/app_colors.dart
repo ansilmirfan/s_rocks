d5c5384c25914be5031c41310260683a339a678f
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-24 12:51:31 +0530

    docs : added comments in app_colors.dart file

--- lib/res/style/app_colors.dart
+++ lib/res/style/app_colors.dart
@@ -1,11 +1,22 @@
 import 'package:flutter/material.dart';
 
 class AppColors {
+  ///Used in secondary text or unselected icon color
   static Color grey = Color(0xFF61616B);
+
+  ///Primary text color
   static Color white = Colors.white;
+
+  ///Main background color
   static Color black = Color(0xFF18171C);
+
+  ///Semi-transperent color used in cards
   static Color transperent = Color(0xFF202126);
+
+  ///Darker shade of grey used in container background
   static Color darkGrey = Color(0xFF2F2F39);
+
+  ///Gradient in banner section
   static LinearGradient linearGradient = LinearGradient(
     begin: Alignment.bottomCenter,
     end: Alignment.topCenter,
