3dd1f2f462b7b41a97894ec561d9480f998dd29e
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-24 12:41:42 +0530

    chore : added app_colors.dart file    -contains colors for the application

---
+++ lib/res/style/app_colors.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static Color grey = Color(0xFF61616B);
+  static Color white = Colors.white;
+  static Color black = Colors.black;
+  static Color transperent = Color(0xFF202126);
+  static Color darkGrey = Color(0xFF2F2F39);
+  static LinearGradient linearGradient = LinearGradient(
+    begin: Alignment.bottomCenter,
+    end: Alignment.topCenter,
+    colors: [Color(0xFF550120), Color(0xFFA90140)],
+  );
+}
