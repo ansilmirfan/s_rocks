f6b7f598621239095ed50c6473e7acbabb91a9ca
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-24 13:00:31 +0530

    feat : added AppColorScheme class using AppColors

---
+++ lib/res/style/colors_scheme.dart
@@ -0,0 +1,28 @@
+import 'package:flutter/material.dart';
+import 'app_colors.dart';
+
+class AppColorScheme {
+  static final ColorScheme appColorScheme = ColorScheme(
+    brightness: Brightness.dark,
+
+    /// Primary colors
+    primary: Color(0xFFA90140),
+    onPrimary: AppColors.white,
+
+    /// Secondary colors
+    secondary: Color(0xFF550120),
+    onSecondary: AppColors.white,
+
+    /// Surface
+    surface: AppColors.transperent,
+    onSurface: AppColors.white,
+
+    /// Error for validation
+    error: Colors.redAccent,
+    onError: AppColors.white,
+
+    /// Tertiary buttons,cards
+    tertiary: AppColors.darkGrey,
+    onTertiary: AppColors.white,
+  );
+}
