ab9a7ef71b43392d7eba3c55a5008598bbda48be
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-25 21:18:16 +0530

    feat: added firebase_core package    -added package in pubspec.yaml file    -added intialising method in main function

--- lib/main.dart
+++ lib/main.dart
@@ -1,8 +1,11 @@
+import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
 import 'package:s_rocks/res/style/app_theme.dart';
 import 'package:s_rocks/view/splash_screen/splash_screen.dart';
 
-void main() {
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
   runApp(const MyApp());
 }
 
