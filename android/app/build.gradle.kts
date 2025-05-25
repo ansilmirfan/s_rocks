08dc3db099dc23f616c109690407d16ee8a95b02
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-25 21:09:27 +0530

    feat : updated files for firebase

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -3,8 +3,13 @@ plugins {
     id("kotlin-android")
     // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
+    id("com.google.gms.google-services")
 }
+dependencies {
+  implementation(platform("com.google.firebase:firebase-bom:33.14.0"))
+  implementation("com.google.firebase:firebase-analytics")
 
+}
 android {
     namespace = "com.example.s_rocks"
     compileSdk = flutter.compileSdkVersion
