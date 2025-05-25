384164c58c0016ac03bf0b05069349f1ec6bf2a0
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-26 02:35:52 +0530

    refactor : Updated code    -added ChangeNotifierProvider    -called dependency inejection in main method

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,16 @@
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:provider/provider.dart';
+import 'package:s_rocks/core/dependency_injection/injector.dart';
+
 import 'package:s_rocks/res/style/app_theme.dart';
 import 'package:s_rocks/view/splash_screen/splash_screen.dart';
+import 'package:s_rocks/view_model/music_service_view_model.dart';
 
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
+  //dependency injection
+  Injector.init();
   await Firebase.initializeApp();
   runApp(const MyApp());
 }
@@ -14,6 +20,10 @@ class MyApp extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(theme: AppTheme.themeData, home: SplashScreen());
+    return ChangeNotifierProvider(
+      create: (context) => Injector.getIt<MusicServiceViewModel>()..getAll(),
+
+      child: MaterialApp(theme: AppTheme.themeData, home: SplashScreen()),
+    );
   }
 }
