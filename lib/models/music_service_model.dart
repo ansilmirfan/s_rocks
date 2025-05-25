79b2587c115ea1c1695325294c9c2dc7f5d7ff5c
ansilmirfan <ansilmirfan123@gmail.com>
2025-05-26 02:30:09 +0530

    refactor : Updated MusicServiceModel class with factory constructor     -factory constructor fromMap converts map into MusicServiceModel

--- lib/models/music_service_model.dart
+++ lib/models/music_service_model.dart
@@ -1,6 +1,9 @@
+import 'dart:developer';
+
 import 'package:flutter/widgets.dart';
 
 class MusicServiceModel {
+  final String id;
   final String title;
   final String description;
   //leading icon asset path
@@ -11,10 +14,29 @@ class MusicServiceModel {
   final Alignment alignment;
 
   MusicServiceModel({
+    required this.id,
     required this.title,
     required this.description,
     required this.leadingIconPath,
     required this.bgImagePath,
     required this.alignment,
   });
+
+  factory MusicServiceModel.fromMap({required Map<String, dynamic> map}) {
+    log(map.toString());
+    final Map<String, num> align = Map<String, num>.from(map['alignment']);
+    final Alignment alignment = Alignment(
+      (align["x"] as num).toDouble(),
+      (align["y"] as num).toDouble(),
+    );
+
+    return MusicServiceModel(
+      id: map['id'],
+      title: map['title'],
+      description: map["description"],
+      leadingIconPath: map["leadingIconPath"],
+      bgImagePath: map["bgImagePath"],
+      alignment: alignment,
+    );
+  }
 }
